dbd9bc7980e9ffe188828091f71ab902c522d488
Aplly05 <angelineapolinar28@gmail.com>
2025-06-23 18:46:03 +0800

    _helpers

---
+++ db.js
@@ -0,0 +1,26 @@
+const config = require('../config.json');
+const mysql = require('mysql2/promise');
+const { Sequelize } = require('sequelize');
+/**const { Sequelize, DataTypes } = require('sequelize');
+const AccountModel = require('../accounts/account.model');
+const RefreshTokenModel = require('../accounts/refresh-token.model');**/
+
+module.exports = db = {};
+
+initialize();
+
+async function initialize() {
+  const { host, port, user, password, database } = config.database;
+  const connection = await mysql.createConnection({ host, port, user, password });
+  await connection.query(`CREATE DATABASE IF NOT EXISTS \`${database}\`;`);
+
+  const sequelize = new Sequelize(database, user, password, { dialect: 'mysql' });
+
+  db.Account = require('../accounts/account.model')(sequelize);
+  db.RefreshToken = require('../accounts/refresh-token.model')(sequelize);
+
+  db.Account.hasMany(db.RefreshToken, { onDelete: 'CASCADE' });
+  db.RefreshToken.belongsTo(db.Account);
+
+  await sequelize.sync({ alter:true });
+}
